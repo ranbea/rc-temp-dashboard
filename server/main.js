46e66a197a9816166af508c8395c305f3e18bcf8
raniceyue <ranicetan68@gmail.com>
2021-04-12 05:37:10 +0800

    Add debug statements to main.js

--- server/main.js
+++ server/main.js
@@ -8,10 +8,16 @@ import * as Papa from 'papaparse';
  */
 Meteor.startup(() => {
     const data_file = Assets.getText('room-temperatures.csv');
+    
     console.log("Parsing data...");
     const parsed_data = Papa.parse(data_file).data;
-    console.log("Parsed " + formatted_data + " rows");
+    console.log("Parsed " + parsed_data.length + " rows");
+    
     console.log("Formatting data... Will take about to 2 minutes");
     const formatted_data = formatData(parsed_data);
-    console.log("Data formatting complete");
+    console.log("Data formatting complete: formatted", formatted_data.length, "items");
+
+    console.log("Inserting data into DB");
+    formatted_data.forEach((d) => Data.insert(d));
+    console.log("Data insertion complete: inserted", Data.find().count(), "items");
 });
